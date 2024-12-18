746e51316f3c566e73933efef354c3c736472746
yaelRedlich <yael054859@gmail.com>
2024-12-18 17:01:56 +0200

    projectCore
    lesson2

---
+++ models/Book.cs
@@ -0,0 +1,16 @@
+namespace lesson1;
+public enum CategoryBooks
+{
+    Biography,    
+    Textbook,   
+    Tension,   
+    History, 
+    Science,    
+    Philosophy  
+}
+public class Book{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public CategoryBooks Category { get; set; }
+    
+}
